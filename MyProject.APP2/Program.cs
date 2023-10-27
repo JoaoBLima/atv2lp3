13bf5184689cea2aaf24d1ac1be1121efa8f3b5e
evimariia <evilam.scarneiro@gmail.com>
2023-10-27 20:47:14 -0300

    Teste do acesso ao banco pela BLL

---
+++ MyProject.APP2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyProject.BLL;
+using MyProject.MODEL;
+
+
+
+Usuario user = new Usuario();
+user = usuarioRepository.GetById(1);
+Console.WriteLine("Nome:" + user.Nome);
